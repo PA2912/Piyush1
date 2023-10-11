d43d9afd7e101c68ea28445774679b5bc5b21c65
PA2912 <132337309+PA2912@users.noreply.github.com>
2023-10-11 21:44:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import streamlit as st 
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
+    
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    print("Hello World!")
+
+if __name__ == '__main__':
+    main()
