4426a885d45f9d6fb7b6fbd27a21bc57d874e36c
PA2912 <132337309+PA2912@users.noreply.github.com>
2023-10-11 21:52:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ def main():
     # upload file
     pdf = st.file_uploader("Upload your PDF", type="pdf")
     print("Hello World!")
+        # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks
 
 if __name__ == '__main__':
     main()
